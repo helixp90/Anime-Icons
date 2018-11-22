af685d20ce093315fac4f5de92c8f5381d2fd487
KUYA <owenndejesus@gmail.com>
2018-11-22 16:28:46 +0800

    motherf**

--- Change Anime Icon Directory2/Program.cs
+++ Change Anime Icon Directory2/Program.cs
@@ -16,33 +16,117 @@ namespace Change_Anime_Icon_Directory
 
             var key = Console.ReadKey().KeyChar;
 
-            //Console.Write(key.ToString());
-
-            if (key.ToString() == "1")
+            try
             {
-                AddIcons();
+                if (key.ToString() == "1")
+                {
+                    AddIcons();
+                }
+
+                else if (key.ToString() == "2")
+                {
+                    UpdateIcons();
+                }
+
+                else
+                {
+                    Console.Write("DAIKON TAKUAN BURI DAIKON, BURI BURI BURI");
+                }
+
+
             }
 
-            else if (key.ToString() == "2")
-            {
-                UpdateIcons();
-            }
-
-            else
-            {
-                Console.Write("DAIKON TAKUAN BURI DAIKON, BURI BURI BURI");
-            }
             
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\n" + e);
 
-            Console.WriteLine($"\nPress any key to terminate...");
+                
+            }
+
+            Console.WriteLine($"\n\nPress any key to terminate...");
             Console.ReadKey();
-
 
         }
 
         private static void AddIcons() //for adding folders with icons for the 1st time
         {
-            //int start, end;
+            int start, end;
+            string filedir = Environment.CurrentDirectory, olddir = null, newdir = null, anime;
+
+            string[] foldername = null;
+
+            IEnumerable<string> ini = null;
+
+            foldername = Directory.GetDirectories(filedir, "*", SearchOption.AllDirectories); // * denotes all (i think)
+
+            foreach (var x in foldername)
+            {
+                anime = Path.GetFileName(x);
+
+                Console.WriteLine(anime);
+
+                ini = Directory.EnumerateFiles(Path.GetDirectoryName(x), "desktop.ini", SearchOption.AllDirectories);
+
+                Console.WriteLine(ini);
+
+                foreach (var deskini in ini)
+                {
+                    File.SetAttributes(deskini, FileAttributes.Archive);
+
+                    foreach (var line in File.ReadAllLines(deskini))
+                    {
+                        if (line.Contains("IconResource"))
+                        {
+                            start = line.IndexOf("icons");
+
+                            Console.WriteLine(line);
+                            Console.WriteLine(line.Length.ToString());
+                            Console.WriteLine(line.IndexOf("icons").ToString());
+
+                            if (line.Contains(","))
+                            {
+                                end = line.IndexOf(",");
+
+                                olddir = line.Substring(start, (end - start));
+
+                                Console.Write(end - start);
+                            }
+
+                            else
+                            {
+                                end = line.Length - 1;
+
+                                Console.Write(end);
+
+                                olddir = line.Substring(start, (end - start + 1));
+
+                                Console.Write(end - start);
+                            }
+
+                            Console.WriteLine("\n" + filedir);
+
+                            newdir = File.ReadAllText(deskini);
+
+                            newdir = newdir.Replace(line, "IconResource=" + filedir + olddir + ",0"); // gets too much slashes when parent
+
+                            File.WriteAllText(deskini, newdir, Encoding.Unicode);
+
+                            Console.WriteLine("\n" + olddir);
+                            Console.WriteLine("\n" + newdir);
+
+                        }
+
+                        else
+                        {
+
+                        }
+
+                    }
+
+                    File.SetAttributes(deskini, FileAttributes.Hidden);
+                }
+            }
         }
 
         private static void UpdateIcons() //for updating folder icons to new directory
@@ -50,6 +134,7 @@ namespace Change_Anime_Icon_Directory
             int start, end;
             string filedir = Environment.CurrentDirectory, olddir = null, newdir = null;
 
+ 
             IEnumerable<string> Ini = Directory.EnumerateFiles(filedir, "desktop.ini", SearchOption.AllDirectories);
 
             foreach (string deskini in Ini)
@@ -86,15 +171,16 @@ namespace Change_Anime_Icon_Directory
                             Console.Write(end - start);
                         }
 
+                        Console.WriteLine("\n" + filedir);
 
                         newdir = File.ReadAllText(deskini);
 
-                        newdir = newdir.Replace(line, "IconResource=" + filedir + "\\" + olddir + ",0");
+                        newdir = newdir.Replace(line, "IconResource=" + filedir + olddir + ",0"); // gets too much slashes when parent
 
                         File.WriteAllText(deskini, newdir, Encoding.Unicode);
 
-                        Console.WriteLine(olddir);
-                        Console.WriteLine(newdir);
+                        Console.WriteLine("\n" + olddir);
+                        Console.WriteLine("\n" + newdir);
 
                     }
 
