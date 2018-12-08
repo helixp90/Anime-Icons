eb63c27f622294d42476baab622f88f6f1beb234
KUYA <owenndejesus@gmail.com>
2018-12-09 01:39:22 +0800

    fuck yesh

--- Change Anime Icon Directory2/Program.cs
+++ Change Anime Icon Directory2/Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 public enum FolderCustomSettingsMask : uint
 {
@@ -25,12 +28,12 @@ public enum FolderCustomSettingsRW : uint
 
 
 
-namespace Change_Anime_Icon_Directory //This program assumes: 1.) Icon Folder is located outside of Anime Folder; 2.) Icon and Anime Folder have the same name
+namespace Change_Anime_Icon_Directory //This program assumes: 1.) Icon Folder is located within Anime Folder directory; 2.) Icon and Anime Folder have the same name
 {
     class Program
     {
 
-        //[SecurityCritical]
+        
         [DllImport("Shell32.dll", CharSet = CharSet.Auto)]
         static extern uint SHGetSetFolderCustomSettings(ref SHFOLDERCUSTOMSETTINGS pfcs, string pszPath, FolderCustomSettingsRW dwReadWrite);
 
@@ -57,47 +60,8 @@ namespace Change_Anime_Icon_Directory //This program assumes: 1.) Icon Folder is
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"Press 1 to add; 2 to update...\n");
 
-            var key = Console.ReadKey().KeyChar;
-
-            try
-            {
-                if (key.ToString() == "1")
-                {
-                    AddIcons();
-                }
-
-                else if (key.ToString() == "2")
-                {
-                    UpdateIcons();
-                }
-
-                else
-                {
-                    Console.Write("DAIKON TAKUAN BURI DAIKON, BURI BURI BURI");
-                }
-
-
-            }
-
-            
-            catch (Exception e)
-            {
-                Console.WriteLine("\n\n" + e);
-
-                
-            }
-
-            Console.WriteLine($"\n\nPress any key to terminate...");
-            Console.ReadKey();
-
-        }
-
-        private static void AddIcons() //for adding folders with icons for the 1st time
-        {
-            int start, end;
-            string filedir = Environment.CurrentDirectory, olddir = null, newdir = null, anime = null;
+            string filedir = Environment.CurrentDirectory, anime = null;
 
             string[] foldername = null, lines = { }, icon = null;
 
@@ -105,201 +69,64 @@ namespace Change_Anime_Icon_Directory //This program assumes: 1.) Icon Folder is
 
             foldername = Directory.GetDirectories(filedir, "*", SearchOption.AllDirectories); // * gets all directories and subdirectories
 
-            foreach (var path in foldername)                                                    // iterates the list of directories
+            Console.WriteLine("Starting...");
+            Thread.Sleep(3000);
+
+            try
             {
-                if (Path.GetFileNameWithoutExtension(path).Contains("icons") == false)
+                foreach (var path in foldername)                                                    // iterates the list of directories
                 {
-                    
-                
-                    Console.WriteLine(path);
-
-                    anime = Path.GetFileNameWithoutExtension(path);                                                // gets the filename of the directory
-
-                    Console.WriteLine("\n\n" + anime);
-
-                    //path = Path.GetDirectoryName(x);                                                           // get the directory name of the file
-
-                    //Console.WriteLine(path);
-
-                    icon = Directory.GetFiles(filedir, anime + ".ico", SearchOption.AllDirectories);                         // finds the icon file with the same name as the directory
-
-                    //Console.WriteLine("\n\n" + filedir);
-                    Console.WriteLine("\n\nICON:" + icon.First());                                  
-
-                    ini = Directory.EnumerateFiles(path, "desktop.ini", SearchOption.AllDirectories); // check if directory has "desktop.ini"
-
-                    
-
-                    foreach (var y in icon)
+                    if (Path.GetFileNameWithoutExtension(path).Contains("icons") == false)
                     {
-                        Console.WriteLine("\n\nINI:" + y);
-                    }
 
 
-                    if (ini.Count() == 0)                                                                                      // if there's no desktop.ini file... create one
-                    {
-                        Console.WriteLine("NULL");
-
-                        /*lines = new string[] { "[ViewState]", "Mode=", "Vid=", "FolderType=Videos", "[.ShellClassInfo]", "IconResource=" + icon.First() + ",0" };
+                        Console.WriteLine("\nFolder path: " + path);                              
 
 
-                        File.WriteAllLines(path + @"\desktop.ini", lines, UnicodeEncoding.Unicode);                     //Creates a desktop ini
-                        File.SetAttributes(path + @"\desktop.ini", FileAttributes.Hidden);*/
+                        anime = Path.GetFileName(path).Replace(".ico", "");                          // gets the filename of the directory
 
-                        SHFOLDERCUSTOMSETTINGS FolderSettings = new SHFOLDERCUSTOMSETTINGS
+                        Console.WriteLine("\nAnime: " + anime);
+
+                       
+
+                        icon = Directory.GetFiles(filedir, anime + ".ico", SearchOption.AllDirectories);                         // finds the icon file with the same name as the directory
+
+                        
+                        Console.WriteLine("\n\nICON: " + icon.First() + "\nlocated at " + path);
+
+                        ini = Directory.EnumerateFiles(path, "desktop.ini", SearchOption.AllDirectories);   // check if directory has "desktop.ini"
+                        
+                        SHFOLDERCUSTOMSETTINGS FolderSettings = new SHFOLDERCUSTOMSETTINGS                  // Create or update desktop.ini using Powershell code
                         {
                             dwMask = FolderCustomSettingsMask.IconFile,
-                            pszIconFile = icon.First(),
+                            pszIconFile = icon.First(),                                                     // desktop.ini folder properties
                             iIconIndex = 0
                         };
 
                         uint hResult = SHGetSetFolderCustomSettings(
-                            ref FolderSettings, path + char.MinValue, FolderCustomSettingsRW.ForceWrite);
+                            ref FolderSettings, path + char.MinValue, FolderCustomSettingsRW.ForceWrite);  //create or overwrite desktop.ini
 
-                        Console.WriteLine(hResult);
-
-                              
-                        File.SetAttributes(path + @"\desktop.ini", FileAttributes.Hidden);
-
+                        Console.WriteLine("\n==================================================================================================");
+                        
                     }
 
-                    else
-                    {
-                        Console.WriteLine("NOT NULL");
-
-                        File.SetAttributes(path + @"\desktop.ini", FileAttributes.Archive);
-
-                        SHFOLDERCUSTOMSETTINGS FolderSettings = new SHFOLDERCUSTOMSETTINGS
-                        {
-                            dwMask = FolderCustomSettingsMask.IconFile,
-                            pszIconFile = icon.First(),
-                            iIconIndex = 0
-                        };
-
-                        uint hResult = SHGetSetFolderCustomSettings(
-                            ref FolderSettings, path + char.MinValue, FolderCustomSettingsRW.ForceWrite);
-
-                        Console.WriteLine(hResult);
-
-                        File.SetAttributes(path + @"\desktop.ini", FileAttributes.Hidden);
-
-                        /*foreach (var deskini in ini)
-                        {
-                            File.SetAttributes(deskini, FileAttributes.Archive);
-
-                            foreach (var line in File.ReadAllLines(deskini))
-                            {
-                                if (line.Contains("IconResource"))
-                                {
-                                    start = line.IndexOf("icons");
-
-                                    Console.WriteLine(line);
-                                    Console.WriteLine(line.Length.ToString());
-                                    Console.WriteLine(line.IndexOf("icons").ToString());
-
-                                    if (line.Contains(","))
-                                    {
-                                        end = line.IndexOf(",");
-
-                                        olddir = line.Substring(start, (end - start));
-
-                                        Console.Write(end - start);
-                                    }
-
-                                    else
-                                    {
-                                        end = line.Length - 1;
-
-                                        Console.Write(end);
-
-                                        olddir = line.Substring(start, (end - start + 1));
-
-                                        Console.Write(end - start);
-                                    }
-
-                                    Console.WriteLine("\n" + filedir);
-
-                                    newdir = File.ReadAllText(deskini);
-
-                                    newdir = newdir.Replace(line, "IconResource=" + filedir + "\\" + olddir + ",0"); // gets too much slashes when parent
-
-                                    File.WriteAllText(deskini, newdir, Encoding.Unicode);
-
-                                    Console.WriteLine("\n" + olddir);
-                                    Console.WriteLine("\n" + newdir);
-
-                                }
-
-                            }
-
-                            File.SetAttributes(deskini, FileAttributes.Hidden);
-                        }*/
-
-                    }
-                    }
-            }
-        }
-
-        private static void UpdateIcons() //for updating folder icons to new directory
-        {
-            int start, end;
-            string filedir = Environment.CurrentDirectory, olddir = null, newdir = null;
-
- 
-            IEnumerable<string> Ini = Directory.EnumerateFiles(filedir, "desktop.ini", SearchOption.AllDirectories);
-
-            foreach (string deskini in Ini)
-            {
-                File.SetAttributes(deskini, FileAttributes.Archive);
-                
-                foreach (var line in File.ReadAllLines(deskini))
-                {
-                    if (line.Contains("IconResource"))
-                    {
-                        start = line.IndexOf("icons");
-
-                        Console.WriteLine(line);
-                        Console.WriteLine(line.Length.ToString());
-                        Console.WriteLine(line.IndexOf("icons").ToString());
-
-                        if (line.Contains(","))
-                        {
-                            end = line.IndexOf(",");
-
-                            olddir = line.Substring(start, (end - start));
-
-                            Console.Write(end - start);
-                        }
-
-                        else
-                        {
-                            end = line.Length - 1;
-
-                            Console.Write(end);
-
-                            olddir = line.Substring(start, (end - start + 1));
-
-                            Console.Write(end - start);
-                        }
-
-                        Console.WriteLine("\n" + filedir);
-
-                        newdir = File.ReadAllText(deskini);
-
-                        newdir = newdir.Replace(line, "IconResource=" + filedir + "\\" + olddir + ",0"); // gets too much slashes when parent
-
-                        File.WriteAllText(deskini, newdir, Encoding.Unicode);
-
-                        Console.WriteLine("\n" + olddir);
-                        Console.WriteLine("\n" + newdir);
-
-                    }
 
                 }
-
-                File.SetAttributes(deskini, FileAttributes.Hidden);
-
             }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+
+
         }
+
     }
+
 }
